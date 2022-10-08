517a9639151928d45b6afa4a78833d8ecc609797
ATZCDPGAL <atzincp@gmail.com>
2022-10-07 19:04:42 -0500

    Agregar archivos de proyecto.

---
+++ NotePad/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotePad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programa creado por Alan Cerda, block de notas para el guardado de texto ", "Acerca de...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Abrir archivo
+            Stream stm;
+            OpenFileDialog ofd= new OpenFileDialog();
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if((stm = ofd.OpenFile())!=null){
+                    string str = ofd.FileName;
+                    string ftxt = File.ReadAllText(str);
+                    richTextBox1.Text = ftxt;
+                }
+            }
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Guardar archivo
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Archivo de texto(*.txt)|*.txt|Archivo de C#(*.cs)|*.cs";
+            sfd.ShowDialog();
+            File.WriteAllText(sfd.FileName,richTextBox1.Text);
+            MessageBox.Show("Archivo de texto guardado exitosamente.");
+        }
+    }
+}
